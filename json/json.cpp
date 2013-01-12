3c2b2c555740c5eeec25dcae184b42b0569d646d
Amir Sabbaghi <asaba90@gmail.com>
2013-01-12 17:09:45 +0330

    initial commit.

---
+++ json/json.cpp
@@ -0,0 +1,279 @@
+#include "json.h"
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+Json::Json()
+{
+	type_ = Json::JTYPE_NULL;
+}
+
+Json::Json(const Json& json)
+{
+	type_ = json.type_;
+	for (std::map<std::string, Json *>::const_iterator i = json.children_.begin(); i != json.children_.end(); i ++)
+		children_.insert(std::pair<std::string, Json *>(i->first, new Json(*i->second)));
+	for (std::vector<Json *>::const_iterator i = json.array_.begin(); i != json.array_.end(); i ++)
+		array_.push_back(new Json(**i));
+	value_ = json.value_;
+}
+
+Json::Json(const std::string& path)
+{
+	load(path);
+}
+
+Json::~Json()
+{
+	for (std::map<std::string, Json *>::iterator i = children_.begin(); i != children_.end(); i ++)
+		delete i->second;
+	for (std::vector<Json *>::iterator i = array_.begin(); i != array_.end(); i ++)
+		delete *i;
+}
+
+void Json::load(const std::string& path)
+{
+	clear();
+	FILE *f = fopen(path.c_str(), "r");
+	if (!f)
+		throw "Json error: cannot open file";
+	fseek(f, 0, SEEK_END);
+	unsigned size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	char *text = new char[size+1];
+	unsigned count = fread(text, 1, size, f);
+	fclose(f);
+	if (count < size)
+	{
+#ifndef _WIN32
+		throw "Json error: file is corrupted";
+#endif
+	}
+	text[count] = 0;
+	rec_parse(text);
+	delete []text;
+}
+
+void Json::save(const std::string& path) const
+{
+	ofstream f(path.c_str());
+	rec_write(f);
+	f.close();
+}
+
+void Json::save(std::string &output) const
+{
+	stringstream ss;
+	rec_write(ss, 0);
+	output = ss.str();
+}
+
+void Json::parse(const std::string& text)
+{
+	rec_parse(text.c_str());
+}
+
+void Json::clear()
+{
+	type_ = Json::JTYPE_NULL;
+	for (std::map<std::string, Json *>::iterator i = children_.begin(); i != children_.end(); i ++)
+		delete i->second;
+	children_.clear();
+	for (std::vector<Json *>::iterator i = array_.begin(); i != array_.end(); i ++)
+		delete *i;
+	array_.clear();
+	value_.clear();
+}
+
+int Json::rec_parse(const char* const text)
+{
+#define SKEEP_SPACES \
+	while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r') \
+		p ++
+	
+	const char *p = text;
+	SKEEP_SPACES;
+	if (*p == 0)
+		return p - text;
+	if (*p == '{')
+	{
+		type_ = Json::JTYPE_OBJECT;
+		while (*p != '}')
+		{
+			p ++;
+			SKEEP_SPACES;
+			// Watch out for an empty object
+			if (*p == '}')
+				break;
+			if (*p != '"')
+				throw "JSON object member name is not a string"; // FIXME
+			string name;
+			p += read_string(p, name);
+			p ++;
+			SKEEP_SPACES;
+			if (*p != ':')
+				throw "JSON object member name does not followed by a colon"; // FIXME
+			p ++;
+			SKEEP_SPACES;
+			Json *child = new Json;
+			p += child->rec_parse(p);
+			p ++;
+			SKEEP_SPACES;
+			if (*p != ',' && *p != '}')
+				throw ("\"}\" or \",\" expected after member \"" + name + "\"").c_str(); // FIXME
+			children_.insert(pair<string, Json*>(name, child));
+		}
+	}
+	else if (*p == '[')
+	{
+		type_ = Json::JTYPE_ARRAY;
+		while (*p != ']')
+		{
+			p ++;
+			SKEEP_SPACES;
+			// Watch out for an empty object
+			if (*p == ']')
+				break;
+			Json *child = new Json;
+			p += child->rec_parse(p);
+			p ++;
+			SKEEP_SPACES;
+			if (*p != ',' && *p != ']')
+				throw "\"]\" or \",\" expected"; // FIXME
+			array_.push_back(child);
+		}
+	}
+	else if (*p == '"')
+	{
+		type_ = Json::JTYPE_STRING;
+		p += read_string(p, value_);
+	}
+	else
+	{
+		for (; *p != ' ' && *p != '\t' && *p != '\n' && *p != '\r' && *p != ',' && *p != ':' && *p != '[' && *p != ']' && *p != '{' && *p != '}'; p ++)
+			value_.push_back(*p);
+		if (value_ == "null")
+			type_ = JTYPE_NULL;
+		else if (value_ == "true" || value_ == "false")
+			type_ = JTYPE_BOOLEAN;
+		else
+			type_ = JTYPE_NUMBER;
+		p --;
+	}
+	return p - text;
+#undef SKEEP_SPACES
+}
+
+int Json::read_string(const char* const text, string& name)
+{
+	const char *p = text;
+	for (p ++; *p && *p != '"'; p ++)
+	{
+		if (*p == '\\')
+		{
+			p ++;
+			switch (*p)
+			{
+			case 'b':
+				name.push_back('\b');
+				break;
+			case 'f':
+				name.push_back('\f');
+				break;
+			case 'n':
+				name.push_back('\n');
+				break;
+			case 'r':
+				name.push_back('\r');
+				break;
+			case 't':
+				name.push_back('\t');
+				break;
+			case 'u':
+			{
+				int num = 0;
+				int i;
+				for (p ++, i = 0; i < 4 && ((*p >= '0' && *p <= '9') || (tolower(*p) >= 'a' && tolower(*p) <= 'f')); p ++)
+					num = num * 16 + (*p >= 'A' ? tolower(*p) - 'a' + 10 : *p - '0');
+				break;
+			}
+			default:
+				name.push_back(*p);
+			}
+		}
+		else
+			name.push_back(*p);
+	}
+	return p - text;
+}
+
+void Json::rec_write(ostream& f, int tab) const
+{
+#define INSERT_TABS \
+	for (int j = 0; j < tab; j ++) \
+		f << '\t'
+	
+	switch (type_)
+	{
+	case Json::JTYPE_OBJECT:
+	{
+		f << "{\n";
+		map<string, Json*>::const_iterator i = children_.begin();
+		if (i != children_.end())
+		{
+			INSERT_TABS;
+			f << "\t\"" << i->first << "\" : ";
+			i->second->rec_write(f, tab+1);
+		}
+		for (i ++; i != children_.end(); i ++)
+		{
+			f << ",\n";
+			INSERT_TABS;
+			f << "\t\"" << i->first << "\" : ";
+			i->second->rec_write(f, tab+1);
+		}
+		f << '\n';
+		INSERT_TABS;
+		f << '}';
+		break;
+	}
+	case Json::JTYPE_ARRAY:
+	{
+		f << "[\n";
+		vector<Json*>::const_iterator i = array_.begin();
+		if (i != array_.end())
+		{
+			INSERT_TABS;
+			f << '\t';
+			(*i)->rec_write(f, tab+1);
+		}
+		for (i ++; i != array_.end(); i ++)
+		{
+			f << ",\n";
+			INSERT_TABS;
+			f << '\t';
+			(*i)->rec_write(f, tab+1);
+		}
+		f << '\n';
+		INSERT_TABS;
+		f << ']';
+		break;
+	}
+	case Json::JTYPE_NULL:
+	case Json::JTYPE_STRING:
+	case Json::JTYPE_NUMBER:
+	case Json::JTYPE_BOOLEAN:
+		f << '"' << value_ << '"';
+		break;
+	default:
+		break;
+	}
+}
+
+template <> std::string Json::as<std::string>() const
+{
+	if (type_ != Json::JTYPE_STRING && type_ != Json::JTYPE_BOOLEAN && type_ != Json::JTYPE_NUMBER)
+		throw "Cannot convert object or array types"; // FIXME: should throw an appropriate exception
+	return value_;
+}
---
+++ json/json.h
@@ -0,0 +1,209 @@
+#ifndef JSON_H
+#define JSON_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+
+class Json
+{
+public:
+	enum Type{
+		JTYPE_NULL,
+		JTYPE_OBJECT,
+		JTYPE_ARRAY,
+		JTYPE_STRING,
+		JTYPE_NUMBER,
+		JTYPE_BOOLEAN
+	};
+
+	Json();
+	Json(const Json& json);
+	Json(const std::string& path);
+	virtual ~Json();
+
+	void load(const std::string& path);
+	void save(const std::string& path) const;
+	void save(std::string& output) const;
+	void parse(const std::string& text);
+	void clear();
+
+	typedef std::map<std::string, Json*>::iterator ObjectIterator;
+	typedef std::map<std::string, Json*>::const_iterator ConstObjectIterator;
+	typedef std::vector<Json*>::iterator ArrayIterator;
+	typedef std::vector<Json*>::const_iterator ConstArrayIterator;
+	inline ObjectIterator objectBegin();
+	inline ConstObjectIterator objectBegin() const;
+	inline ObjectIterator objectEnd();
+	inline ConstObjectIterator objectEnd() const;
+	inline ArrayIterator arrayBegin();
+	inline ConstArrayIterator arrayBegin() const;
+	inline ArrayIterator arrayEnd();
+	inline ConstArrayIterator arrayEnd() const;
+
+	inline Type getType() const;
+	inline void setType(const Type type);
+	inline unsigned size() const;
+
+	// array access
+	inline Json& operator [](unsigned index);
+	inline const Json& operator [](unsigned index) const;
+	// object access
+	inline ConstObjectIterator find(const std::string& key) const;
+	inline Json& operator [](const std::string& key);
+	inline const Json& operator [](const std::string& key) const;
+
+	inline Json& operator =(const Json& json);
+	template <class T> T as() const;
+
+protected:
+	Type type_;
+	std::map<std::string, Json*> children_;
+	std::vector<Json*> array_;
+	std::string value_;
+
+	int rec_parse(const char* const text);
+	int read_string(const char* const text, std::string& name);
+	void rec_write(std::ostream& f, int tab = 0) const;
+};
+
+// inline implementation
+Json::ObjectIterator Json::objectBegin()
+{
+	return children_.begin();
+}
+
+Json::ConstObjectIterator Json::objectBegin() const
+{
+	return children_.begin();
+}
+
+Json::ObjectIterator Json::objectEnd()
+{
+	return children_.end();
+}
+
+Json::ConstObjectIterator Json::objectEnd() const
+{
+	return children_.end();
+}
+
+Json::ArrayIterator Json::arrayBegin()
+{
+	return array_.begin();
+}
+
+Json::ConstArrayIterator Json::arrayBegin() const
+{
+	return array_.begin();
+}
+
+Json::ArrayIterator Json::arrayEnd()
+{
+	return array_.end();
+}
+
+Json::ConstArrayIterator Json::arrayEnd() const
+{
+	return array_.end();
+}
+
+Json::Type Json::getType() const
+{
+	return type_;
+}
+
+void Json::setType(const Json::Type type)
+{
+	if (type_ != type)
+	{
+		clear();
+		type_ = type;
+	}
+}
+
+unsigned Json::size() const
+{
+	switch (type_)
+	{
+	case JTYPE_ARRAY:
+		return array_.size();
+	case JTYPE_OBJECT:
+		return children_.size();
+	case JTYPE_NULL:
+		return 0;
+	default:
+		return 1;
+	}
+}
+
+Json& Json::operator[](unsigned int index)
+{
+	if (type_ != Json::JTYPE_ARRAY)
+	{
+		clear();
+		type_ = Json::JTYPE_ARRAY;
+	}
+	if (index >= array_.size())
+		array_.resize(index + 1);
+	return *array_[index];
+}
+
+const Json& Json::operator[](unsigned int index) const
+{
+	if (type_ != Json::JTYPE_ARRAY)
+		throw "This JSON value is not an array"; // FIXME: should throw an appropriate exception
+	return *array_[index];
+}
+
+Json::ConstObjectIterator Json::find(const std::string& key) const
+{
+	if (type_ != Json::JTYPE_OBJECT)
+		throw "This JSON value is not an object"; // FIXME: should throw an appropriate exception
+	return children_.find(key);
+}
+
+Json& Json::operator[](const std::string& key)
+{
+	if (type_ != Json::JTYPE_OBJECT)
+	{
+		clear();
+		type_ = Json::JTYPE_OBJECT;
+	}
+	if (children_.find(key) == children_.end())
+		children_[key] = new Json();
+	return *children_[key];
+}
+
+const Json& Json::operator[](const std::string& key) const
+{
+	if (type_ != Json::JTYPE_OBJECT)
+		throw "This JSON value is not an object"; // FIXME: should throw an appropriate exception
+	return *children_.find(key)->second;
+}
+
+Json& Json::operator=(const Json& json)
+{
+	clear();
+	type_ = json.type_;
+	for (std::map<std::string, Json *>::const_iterator i = json.children_.begin(); i != json.children_.end(); i ++)
+		children_.insert(std::pair<std::string, Json *>(i->first, new Json(*i->second)));
+	for (std::vector<Json *>::const_iterator i = json.array_.begin(); i != json.array_.end(); i ++)
+		array_.push_back(new Json(**i));
+	value_ = json.value_;
+	return (*this);
+}
+
+template <class T> T Json::as() const
+{
+	if (type_ != Json::JTYPE_STRING && type_ != Json::JTYPE_BOOLEAN && type_ != Json::JTYPE_NUMBER)
+		throw "Cannot convert object or array types"; // FIXME: should throw an appropriate exception
+	std::stringstream ss;
+	ss << value_;
+	T r;
+	ss >> r;
+	return r;
+}
+
+#endif // JSON_H
